aea7b4df933ee8802aa306b4656d613c969f05a8
Chase Hartsell <cmhartsell@me.com>
2024-03-27 15:06:01 -0500

    Implement SDL rendering

---
+++ Platform.cc
@@ -0,0 +1,53 @@
+#include "SDL.h"
+#include <iostream>
+
+#include "Platform.h"
+
+Platform::Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight) {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow(title, 0, 0, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, textureWidth, textureHeight);
+}
+
+Platform::~Platform() {
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+void Platform::Update(void const* buffer, int pitch) {
+    SDL_UpdateTexture(texture, nullptr, buffer, pitch);
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+    SDL_RenderPresent(renderer);
+}
+
+bool Platform::ProcessInput(uint8_t* keys) {
+    SDL_Event event;
+    SDL_Keycode key;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+            {
+                return true;
+            }
+            case SDL_KEYDOWN:
+            {
+                // Mark the key as pressed.
+                key = event.key.keysym.sym;
+                if (keymap.count(key) > 0) keys[keymap.at(key)] = 1;
+            }
+            case SDL_KEYUP:
+            {
+                // Mark the key as not pressed.
+                key = event.key.keysym.sym;
+                if (keymap.count(key) > 0) keys[keymap.at(key)] = 0;
+            }
+        }
+    }
+
+    return false;
+}
---
+++ Platform.h
@@ -0,0 +1,28 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+#include <cstdint>
+#include <SDL_video.h>
+#include <SDL_render.h>
+#include <map>
+#include <SDL_keycode.h>
+
+class Platform {
+    public:
+        Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight);
+        ~Platform();
+        void Update(void const* buffer, int pitch);
+        bool ProcessInput(uint8_t* keys);
+
+    private:
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+        SDL_Texture* texture;
+        const std::map<SDL_Keycode, uint8_t> keymap = {
+            {SDLK_x, 0},   {SDLK_1, 1},   {SDLK_2, 2},   {SDLK_3, 3},
+            {SDLK_q, 4},   {SDLK_w, 5},   {SDLK_e, 6},   {SDLK_a, 7},
+            {SDLK_s, 8},   {SDLK_d, 9},   {SDLK_z, 0xA}, {SDLK_c, 0xB},
+            {SDLK_4, 0xC}, {SDLK_r, 0xD}, {SDLK_f, 0xE}, {SDLK_v, 0xF}};
+};
+
+#endif
